be3abd7b6acb90f42faa8e3d054f8c027f190029
Mahendra <MahendraC@TitanEd.com>
2024-11-27 04:28:19 +0000

    add: course name in admin panel

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,12 +1,13 @@
 import csv
 from django.contrib import admin
 from django.http import HttpResponse
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from .models import *
 
 
 class FeedbackAdmin(admin.ModelAdmin):
     raw_id_fields = ["user"]
-    list_display = ["course_key", "user", "block_name", "rating", "feedback"]
+    list_display = ["course_key","get_course_name", "user", "block_name", "rating", "feedback"]
     search_fields = [
         "course_key",
         "user__username",
@@ -14,9 +15,19 @@ class FeedbackAdmin(admin.ModelAdmin):
     list_filter = ["course_key"]
     actions = ("export_as_csv",)
 
+
+    def get_course_name(self, instance):
+        try:
+            course_overview = CourseOverview.get_from_id(instance.course_key)
+            return course_overview.display_name
+        except Exception as e:
+            return ""
+    get_course_name.short_description = "Course Name"
+
     def export_as_csv(self, request, queryset):
         field_names = [
             "Course ID",
+            "Course Name",
             "User",
             "Email",
             "Feedback Block",
@@ -36,8 +47,16 @@ class FeedbackAdmin(admin.ModelAdmin):
                 )
             except Exception as e:
                 fullname = obj.user.username
+
+            try:
+                course_overview = CourseOverview.get_from_id(obj.course_key)
+                course_name = course_overview.display_name
+            except Exception as e:
+                course_name = ""
+
             data = [
                 obj.course_key,
+                course_name,
                 fullname,
                 obj.user.email,
                 obj.block_name,
